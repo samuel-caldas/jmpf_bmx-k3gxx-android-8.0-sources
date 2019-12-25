5c61daefdf5241a3e8c9efd1fe4b286610c6a4cb
jmpfbmx <jmpf.bmx@gmail.com>
2019-12-25 16:41:40 +0100

    Fix Android 8.0 Sources

---
+++ device/samsung/k3gxx/vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo lineage_k3gxx-eng
+add_lunch_combo lineage_k3gxx-userdebug
---
+++ hardware/samsung/wifiloader/Android.mk
@@ -0,0 +1,25 @@
+ifeq ($(BOARD_HAVE_SAMSUNG_WIFI),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    wifiloader.c
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog libutils
+
+ifneq ($(WIFI_DRIVER_MODULE_NAME),)
+LOCAL_CFLAGS += -DWIFI_DRIVER_MODULE_NAME=\"$(WIFI_DRIVER_MODULE_NAME)\"
+endif
+
+ifneq ($(WIFI_DRIVER_MODULE_PATH),)
+LOCAL_CFLAGS += -DWIFI_DRIVER_MODULE_PATH=\"$(WIFI_DRIVER_MODULE_PATH)\"
+endif
+
+LOCAL_MODULE := wifiloader
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+endif
